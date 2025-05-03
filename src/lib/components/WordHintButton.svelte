eade321669abe0e9734b953d3edc2cebb8e59c60
Jesse Kimotho <jnjug.97@gmail.com>
2025-05-03 13:27:57 +0300

    So far so good

--- src/lib/components/WordHintButton.svelte
+++ src/lib/components/WordHintButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { hintTarget, foundWords, hintFlashWord } from '$lib/stores/game';
+	import { fade, scale } from 'svelte/transition';
 
 	export let word: string;
 	export let grid: string[][];
@@ -23,13 +24,17 @@
 
 {#if isFound}
 	<span
-		class="text-md inline-flex h-6 items-center justify-center rounded-4xl bg-[#26c04c] px-8 text-center font-bold text-white transition-all"
-		class:animate-hintflash={isFlashing}>{word}</span
+		in:scale={{ duration: 300 }}
+		out:fade={{ duration: 150 }}
+		class="text-md inline-flex h-6 items-center justify-center rounded-4xl bg-[#26c04c] px-8 font-bold text-white transition-all"
+		class:flashing={isFlashing}
 	>
+		{word}
+	</span>
 {:else}
 	<button
 		on:click={showHint}
-		class="inline-flex h-6 items-center justify-center rounded-4xl bg-gray-200 text-center text-lg text-black"
+		class="inline-flex h-6 items-center justify-center rounded-4xl bg-gray-200 text-lg text-black"
 		style="width: calc(({word.length}ch * 1.75) + 16px)"
 	>
 		{word.length}
@@ -38,17 +43,17 @@
 
 <style>
 	@keyframes hintflash {
-		0% {
-			transform: scale(1);
-		}
-		50% {
-			transform: scale(1.2);
-		}
+		0%,
 		100% {
 			transform: scale(1);
+			box-shadow: 0 0 0 rgba(0, 0, 0, 0);
+		}
+		50% {
+			transform: scale(1.15);
+			box-shadow: 0 0 6px rgba(38, 192, 76, 0.8);
 		}
 	}
-	.animate-hintflash {
+	.flashing {
 		animation: hintflash 600ms ease-in-out;
 	}
 </style>
--- src/lib/data/quizzes.ts
+++ src/lib/data/quizzes.ts
@@ -250,5 +250,685 @@ export const quizzes = [
     }
   ]
 }
+  },
+{
+  id: 3,
+  title: 'Election Heat 🗳️🥵',
+  author: 'jesse',
+  grid: [["P","C","L","S"],["B","O","T","L"],["V","E","E","A"],["D","I","B","S"]],
+  solutions: ["VOTE","IEBC","BALLOT","DEBATE","POLLS","SEATS"],
+  paths: {
+  "VOTE": [
+    {
+      "row": 2,
+      "col": 0
+    },
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 2
+    }
+  ],
+  "IEBC": [
+    {
+      "row": 3,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 0
+    },
+    {
+      "row": 0,
+      "col": 1
+    }
+  ],
+  "BALLOT": [
+    {
+      "row": 3,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 3
+    },
+    {
+      "row": 1,
+      "col": 3
+    },
+    {
+      "row": 0,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 2
+    }
+  ],
+  "DEBATE": [
+    {
+      "row": 3,
+      "col": 0
+    },
+    {
+      "row": 2,
+      "col": 1
+    },
+    {
+      "row": 3,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 3
+    },
+    {
+      "row": 1,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 2
+    }
+  ],
+  "POLLS": [
+    {
+      "row": 0,
+      "col": 0
+    },
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 0,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 3
+    },
+    {
+      "row": 0,
+      "col": 3
+    }
+  ],
+  "SEATS": [
+    {
+      "row": 3,
+      "col": 3
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 3
+    },
+    {
+      "row": 1,
+      "col": 2
+    },
+    {
+      "row": 0,
+      "col": 3
+    }
+  ]
+}
+  },
+{
+  id: 4,
+  title: 'Shamba Life',
+  author: 'jesse',
+  grid: [["M","D","I","S"],["E","R","N","C"],["E","I","A","K"],["W","F","L","E"]],
+  solutions: ["RAIN","FEED","SICKLE","CALF","WIND","WEED","FARM","KALE","KNIFE","RAKE"],
+  paths: {
+  "RAIN": [
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 2
+    }
+  ],
+  "FEED": [
+    {
+      "row": 3,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 0
+    },
+    {
+      "row": 1,
+      "col": 0
+    },
+    {
+      "row": 0,
+      "col": 1
+    }
+  ],
+  "SICKLE": [
+    {
+      "row": 0,
+      "col": 3
+    },
+    {
+      "row": 0,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 3
+    },
+    {
+      "row": 2,
+      "col": 3
+    },
+    {
+      "row": 3,
+      "col": 2
+    },
+    {
+      "row": 3,
+      "col": 3
+    }
+  ],
+  "CALF": [
+    {
+      "row": 1,
+      "col": 3
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 3,
+      "col": 2
+    },
+    {
+      "row": 3,
+      "col": 1
+    }
+  ],
+  "WIND": [
+    {
+      "row": 3,
+      "col": 0
+    },
+    {
+      "row": 2,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 2
+    },
+    {
+      "row": 0,
+      "col": 1
+    }
+  ],
+  "WEED": [
+    {
+      "row": 3,
+      "col": 0
+    },
+    {
+      "row": 2,
+      "col": 0
+    },
+    {
+      "row": 1,
+      "col": 0
+    },
+    {
+      "row": 0,
+      "col": 1
+    }
+  ],
+  "FARM": [
+    {
+      "row": 3,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 0,
+      "col": 0
+    }
+  ],
+  "KALE": [
+    {
+      "row": 2,
+      "col": 3
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 3,
+      "col": 2
+    },
+    {
+      "row": 3,
+      "col": 3
+    }
+  ],
+  "KNIFE": [
+    {
+      "row": 2,
+      "col": 3
+    },
+    {
+      "row": 1,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 1
+    },
+    {
+      "row": 3,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 0
+    }
+  ],
+  "RAKE": [
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 3
+    },
+    {
+      "row": 3,
+      "col": 3
+    }
+  ]
+}
+  },
+{
+  id: 5,
+  title: 'Safari Sunrise',
+  author: 'jesse',
+  grid: [["N","T","E","E"],["U","R","U","B"],["R","H","O","M"],["I","A","S","A"]],
+  solutions: ["SHRUB","ROAR","TREE","BOMA","OUTRUN","MASAI","HUNTER","SAMBURU","HORN"],
+  paths: {
+  "SHRUB": [
+    {
+      "row": 3,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 3
+    }
+  ],
+  "ROAR": [
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 3,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 0
+    }
+  ],
+  "TREE": [
+    {
+      "row": 0,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 0,
+      "col": 2
+    },
+    {
+      "row": 0,
+      "col": 3
+    }
+  ],
+  "BOMA": [
+    {
+      "row": 1,
+      "col": 3
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 3
+    },
+    {
+      "row": 3,
+      "col": 3
+    }
+  ],
+  "OUTRUN": [
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 2
+    },
+    {
+      "row": 0,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 0
+    },
+    {
+      "row": 0,
+      "col": 0
+    }
+  ],
+  "MASAI": [
+    {
+      "row": 2,
+      "col": 3
+    },
+    {
+      "row": 3,
+      "col": 3
+    },
+    {
+      "row": 3,
+      "col": 2
+    },
+    {
+      "row": 3,
+      "col": 1
+    },
+    {
+      "row": 3,
+      "col": 0
+    }
+  ],
+  "HUNTER": [
+    {
+      "row": 2,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 0
+    },
+    {
+      "row": 0,
+      "col": 0
+    },
+    {
+      "row": 0,
+      "col": 1
+    },
+    {
+      "row": 0,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 1
+    }
+  ],
+  "SAMBURU": [
+    {
+      "row": 3,
+      "col": 2
+    },
+    {
+      "row": 3,
+      "col": 3
+    },
+    {
+      "row": 2,
+      "col": 3
+    },
+    {
+      "row": 1,
+      "col": 3
+    },
+    {
+      "row": 1,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 0
+    }
+  ],
+  "HORN": [
+    {
+      "row": 2,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 0,
+      "col": 0
+    }
+  ]
+}
+  },
+{
+  id: 6,
+  title: 'Nairobi Hoods 1 ',
+  author: 'jesse',
+  grid: [["T","H","O","K"],["A","U","A","M"],["W","M","L","E"],["I","A","U","R"]],
+  solutions: ["KAMULU","THOME","KAHAWA","RUAI","UTAWALA"],
+  paths: {
+  "KAMULU": [
+    {
+      "row": 0,
+      "col": 3
+    },
+    {
+      "row": 1,
+      "col": 2
+    },
+    {
+      "row": 2,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 3,
+      "col": 2
+    }
+  ],
+  "THOME": [
+    {
+      "row": 0,
+      "col": 0
+    },
+    {
+      "row": 0,
+      "col": 1
+    },
+    {
+      "row": 0,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 3
+    },
+    {
+      "row": 2,
+      "col": 3
+    }
+  ],
+  "KAHAWA": [
+    {
+      "row": 0,
+      "col": 3
+    },
+    {
+      "row": 1,
+      "col": 2
+    },
+    {
+      "row": 0,
+      "col": 1
+    },
+    {
+      "row": 1,
+      "col": 0
+    },
+    {
+      "row": 2,
+      "col": 0
+    },
+    {
+      "row": 3,
+      "col": 1
+    }
+  ],
+  "RUAI": [
+    {
+      "row": 3,
+      "col": 3
+    },
+    {
+      "row": 3,
+      "col": 2
+    },
+    {
+      "row": 3,
+      "col": 1
+    },
+    {
+      "row": 3,
+      "col": 0
+    }
+  ],
+  "UTAWALA": [
+    {
+      "row": 1,
+      "col": 1
+    },
+    {
+      "row": 0,
+      "col": 0
+    },
+    {
+      "row": 1,
+      "col": 0
+    },
+    {
+      "row": 2,
+      "col": 0
+    },
+    {
+      "row": 3,
+      "col": 1
+    },
+    {
+      "row": 2,
+      "col": 2
+    },
+    {
+      "row": 1,
+      "col": 2
+    }
+  ]
+}
 },
 ];
